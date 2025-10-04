8b0c02d8a7c029d99ad4e455b41c054106433e0b
Nur <nur.alam@amharctech.com>
2025-10-05 01:14:08 +0600

    remove production url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:3000/api",
-  baseURL: "https://task-management-production-ca51.up.railway.app/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
